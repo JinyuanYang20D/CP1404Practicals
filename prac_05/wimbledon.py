39ca8774b1629c7f9dd206fcde97dc3bf7af20fc
yangjinyuan <jinyuan.yang@my.jcu.edu.au>
2023-09-03 19:55:24 +0800

    Add another language to the file.

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,9 @@
+"""
+wimbledon
+Estimate: 20 minutes
+Actual:   35 minutes
+"""
+
+def main():
+    """Apply various functions to display requested results."""
+    filename = "wimbledon.csv"
---
+++ prac_07/programming_language.py
@@ -0,0 +1,48 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language class with tests.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, method_overloading):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.method_overloading = method_overloading
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Method Overloading is {self.method_overloading}."
+
+    def __str__(self):
+        """Return the string form of the class."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, Method Overloading is {self.method_overloading}."
+
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, "Yes")
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, "Yes")
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, "No")
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
